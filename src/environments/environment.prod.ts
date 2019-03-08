d03b94f77fcd3509c0b95c05de422367dae13fcd
gandikotamadan <madanpro321@gmail.com>
2019-03-08 10:16:16 +0530

    My Project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyClH4bm4C8TS5bLGjTqBgUYtI3v-Tx8glg",
+    authDomain: "weather-54942.firebaseapp.com",
+    databaseURL: "https://weather-54942.firebaseio.com",
+    projectId: "weather-54942",
+    storageBucket: "weather-54942.appspot.com",
+    messagingSenderId: "528320972691"
+  }
+};
